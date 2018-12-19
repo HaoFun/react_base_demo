e99d3abe82565fa7b9b6f77fe5a198aac0b7a399
Hao-Chang <hao.chang@contin-global.com>
2018-12-19 10:25:18 +0800

    first commit

---
+++ precache-manifest.f0f8f99fd842fd852bf6e40ddeebd1d9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f0247d8c85cbc3d9956",
+    "url": "/static/js/5.4f0247d8.chunk.js"
+  },
+  {
+    "revision": "7f863ee484a7311e165c",
+    "url": "/static/css/main.4e8d8277.chunk.css"
+  },
+  {
+    "revision": "0cf759a626e5b49c1b6d",
+    "url": "/static/js/1.0cf759a6.chunk.js"
+  },
+  {
+    "revision": "09bb16bd09195445bc3a",
+    "url": "/static/js/2.09bb16bd.chunk.js"
+  },
+  {
+    "revision": "315818786a98c02fd49c",
+    "url": "/static/js/3.31581878.chunk.js"
+  },
+  {
+    "revision": "d18217c547abefcb2151",
+    "url": "/static/js/4.d18217c5.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7f863ee484a7311e165c",
+    "url": "/static/js/main.7f863ee4.chunk.js"
+  },
+  {
+    "revision": "a51c2b424accb09d18ce",
+    "url": "/static/js/runtime~main.a51c2b42.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "4f0247d8c85cbc3d9956",
+    "url": "/static/css/5.846a34ed.chunk.css"
+  },
+  {
+    "revision": "d0c56cff1d614a3b79c4645852a09e73",
+    "url": "/index.html"
+  }
+];
